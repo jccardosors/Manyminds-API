b33a31751b0e5d817907fbca9123c59e0c97921d
Jose Cardoso (Externo) <jose.cardoso.externo@ageas.pt>
2024-04-26 16:16:06 -0300

    Implementação de teste unitario e teste de integração

---
+++ Manyminds.Tests/Integration/UsuarioTest.cs
@@ -0,0 +1,92 @@
+﻿using Manyminds.Application.ViewModels.Request.Usuario;
+using Manyminds.Application.ViewModels.Response;
+using Manyminds.Application.ViewModels.Response.Usuario;
+using Manyminds.Domain.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Manyminds.Tests.Integration
+{
+    public class UsuarioTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private HttpClient _client;
+
+        public UsuarioTest(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient();
+
+            Autenticar();
+        }
+
+        protected void Autenticar()
+        {
+            var loginRequest = new LoginRequest
+            {
+                Email = "ze@gmail.com",
+                Senha = "senha123"
+            };
+
+            var autenticarResponse = _client.PostAsJsonAsync("https://localhost:7124/api/Autenticacao/Autenticar", loginRequest).Result;
+            var content = autenticarResponse.Content.ReadAsStringAsync().Result;
+            var result = JsonConvert.DeserializeObject<LoginResponse>(content);
+            _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", result!.Data.Token);
+        }
+
+        [Fact(DisplayName = "RetornarTodosUsuarios")]
+        public async Task GetUsers()
+        {
+            //arrange
+            var url = "https://localhost:7124/api/Usuario/RetornarTodosUsuarios";
+
+            //act
+            var response = await _client.GetAsync(url);
+
+            //assert
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<RetornarTodosResponse>(content);
+
+            Assert.True(response.IsSuccessStatusCode);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.True(result!.Data.Any());
+        }
+              
+        [Theory(DisplayName = "RetornarUsuario")]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async Task RetornarUsuario(int id)
+        {
+            //arrange
+            var url = "https://localhost:7124/api/Usuario/RetornarUsuario?codigo=";
+
+            //act
+            var response = await _client.GetAsync($"{url}{id}");
+
+            //assert
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<UsuarioResponse>(content);
+
+            Assert.True(response.IsSuccessStatusCode);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal((int)HttpStatusCode.OK, result!.Status);
+            Assert.True(result!.Success);
+            Assert.NotNull(result!.Data);
+            Assert.NotEmpty(result.Data.Email);
+        }
+
+    }
+}
---
+++ Manyminds.Tests/Unit/Controllers/UsuarioControllerTest.cs
@@ -0,0 +1,63 @@
+﻿using Manyminds.Api.Controllers;
+using Manyminds.Application.Interfaces;
+using Manyminds.Application.ViewModels;
+using Manyminds.Application.ViewModels.Response;
+using Manyminds.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Manyminds.Tests.Unit.Controllers
+{
+    public class UsuarioControllerTest
+    {
+        private readonly UsuarioController _controller;
+        private readonly Mock<ILogger<UsuarioController>> _mocklogger = new();
+        private readonly Mock<IUsuarioService> _mockUsuarioService = new();
+
+        public UsuarioControllerTest()
+        {
+            _controller = new UsuarioController(_mocklogger.Object, _mockUsuarioService.Object);
+        }
+
+        [Fact(DisplayName = "RetornarTodosUsuarios")]
+        public async Task GetUsers()
+        {
+            //arrange
+            _mockUsuarioService.Setup(p => p.RetornarTodos()).ReturnsAsync(new RetornarTodosResponse
+            {
+                Data = new List<UsuarioVM>
+                 {
+                     new UsuarioVM { Codigo = 1, Email = "teste@gmail.com" }
+                 },
+                Message = "",
+                Status = 200,
+            });
+
+            //act
+            var response = await _controller.RetornarTodosUsuarios();
+
+
+            //assert
+            var responseResult = Assert.IsType<OkObjectResult>(response.Result);   
+            var objectResult = Assert.IsAssignableFrom<RetornarTodosResponse>(responseResult.Value);
+
+            Assert.Equal((int)HttpStatusCode.OK, responseResult.StatusCode);
+            Assert.True(objectResult.Data.Any());
+            Assert.Equal((int)HttpStatusCode.OK, objectResult.Status);
+            Assert.True(objectResult.Success);
+        }
+
+
+    }
+}
